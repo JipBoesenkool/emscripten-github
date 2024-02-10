78e2719d29133086cc9ae304ff2badf9eebbca4c
JipBoesenkool <JipBoesenkool@users.noreply.github.com>
2024-02-10 17:12:40 +0000

    Deploying to gh-pages-TemplateApp from @ JipBoesenkool/emscripten-github@958de2c467ca8a8bad39cd1cd3759e5c90c772cd 🚀

--- exe/cmake_install.cmake
+++ exe/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/runner/work/emscripten-github/emscripten-github/engine/exe
+# Install script for directory: /home/runner/work/emscripten-github/emscripten-github/exe
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,11 +35,6 @@ endif()
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/runner/work/emscripten-github/emscripten-github/apps/TemplateApp/build/exe/libs/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
