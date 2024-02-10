d047798ecfb73f71af93d284d0ca64118b9056a9
JipBoesenkool <JipBoesenkool@users.noreply.github.com>
2024-02-10 15:52:28 +0000

    Deploying to gh-pages-TemplateApp from @ JipBoesenkool/emscripten-github@020005b2088f4ef33e530b6298c7b609f1fb3c89 🚀

---
+++ CPM_modules/Findimgui.cmake
@@ -0,0 +1,3 @@
+include(/home/runner/work/emscripten-github/emscripten-github/cmake/.cpmcache/cpm/CPM_0.32.0.cmake)
+CPMAddPackage(NAME;imgui;GITHUB_REPOSITORY;ocornut/imgui;VERSION;1.84.1;DOWNLOAD_ONLY)
+set(imgui_FOUND TRUE)
